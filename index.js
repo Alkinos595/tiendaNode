fe5354b2af68b058526b10097319223459eaa1b5
Alkinos595 <foinquinosalberto@gmail.com>
2023-01-16 22:54:12 -0400

    commit 4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const routerApi = require('./routes/routersIndex.js')
 const {logErrors,errorHandler,boomErrorHandler} = require('./middlewares/errorHandler.js')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
